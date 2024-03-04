2e37f363030d4a9d0bbc30d3be6e6d80b7e070b7
Mariem Abdalwahab <mariem2562003@gmail.com>
2024-03-05 00:09:08 +0200

    Recipe

--- js/main.js
+++ js/main.js
@@ -7,9 +7,11 @@ let showSaved = document.querySelector('.showSaved');
 let saved = document.querySelector('.saved');
 let savedBoxs = document.querySelector('.saved .boxs');
 let home = document.querySelector('.home');
+let message = document.querySelector('.message');
 
 
 let arr;
+let arrId;
 // let idMeals;
 if(localStorage.getItem('item')){
     arr = (JSON.parse(localStorage.getItem('item')));
@@ -17,7 +19,14 @@ if(localStorage.getItem('item')){
 else{
     arr = [];
 }
-console.log(arr);
+
+if(localStorage.getItem('itemid')){
+    arrId = (JSON.parse(localStorage.getItem('itemid')));
+}
+else{
+    arrId = [];
+}
+
 
 btnSearch.addEventListener('click',searchMeal);
 result.addEventListener('click',mealDetails);
@@ -25,7 +34,7 @@ saved.addEventListener('click',mealDetails);
 
 function searchMeal(){
     result.innerHTML = "";
-    console.log('l')
+   
     let val = search.value.trim();
     if(val){
         let api1 = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${val}`;
@@ -33,6 +42,7 @@ function searchMeal(){
         .then(response=>{
            return response.json();
         }).then(data=>{
+            console.log(data);
             if(data.meals){
                 let boxs = data.meals.map((meal)=>{
                     return `
@@ -66,7 +76,7 @@ let ele;
 function mealDetails(e){
     e.preventDefault();
     if(e.target.classList.contains('mealButton')){
-        console.log('f')
+       
         let id = e.target.id;
         ele = e.target.parentElement.parentElement;
 
@@ -82,7 +92,7 @@ function mealDetails(e){
             let recipe = `
             <div class="main">
               <div class="button d-flex justify-content-between">
-                <i class="fa-regular fa-bookmark save"></i>
+                <i class="fa-regular fa-bookmark save " data-id=${val.idMeal}></i>
                 <i class = "fas fa-times close"></i>
               </div>
               <h4 class="name">${val.strMeal}</h4>
@@ -97,7 +107,7 @@ function mealDetails(e){
             `;
             details.style.display = 'block';
             details.innerHTML = recipe;
-            console.log(details)
+            
         })
         
     }
@@ -114,20 +124,64 @@ function closeRecipe(e){
     }
 }
 
-       
+
+let checkvar = true, index = null;
 function saveItem(e){
     if(e.target.classList.contains('save')){
         // console.log(ele.dataset.id,'kknjkjnkn');
        
         // idMeals.push(ele.dataset.id);
         console.log(arr)
-        arr.push(ele.innerHTML);
+        console.log(e.target.dataset.id);
+        checkID(e.target.dataset.id);
+        if(checkvar != false){
+            arr.push(ele.innerHTML);
+            arrId.push(e.target.dataset.id);
+            console.log(arrId);
+            message.innerHTML = "Recipe Saved";
+            message.style.opacity = 1;
+            setTimeout((e) => {
+            message.style.opacity = 0;
+                
+            }, 1500);
+        }else{
+            arrId.splice(index,1);
+            arr.splice(index,1);
+            console.log(index);
+            index = null;
+            console.log(arrId);
+
+
+            message.innerHTML = "Recipe Un Saved";
+            message.style.opacity = 1;
+            setTimeout((e) => {
+            message.style.opacity = 0;
+                
+            }, 1500);
+
+        }
+        
         // console.log(arr)
         localStorage.setItem('item',JSON.stringify(arr));
+        localStorage.setItem('itemid',JSON.stringify(arrId));
         // console.log(localStorage.getItem('item'))
     }
     
 }
+// localStorage.clear();
+// check Id
+function checkID(ele){
+    arrId.forEach((element,i) => {
+        if(element == ele){
+            checkvar = false;
+            index = i;
+            return;
+        }else{
+            checkvar = true;
+        }
+    });
+}
+
 
 showSaved.addEventListener('click',show);
 
@@ -142,8 +196,7 @@ function show(e){
                 </div>`;
     }).join('');
     saved.style.display = 'block'
-    console.log(y);
-    console.log(savedBoxs);
+
     
     container.style.display = 'none'
     home.style.display = 'block'
@@ -157,11 +210,3 @@ home.addEventListener('click',()=>{
 
 })
 
-// localStorage.clear();
-
-// if(localStorage.getItem('item')){
-
-// }
-// else{
-//     savedItem.style.display = 'none';
-// }
